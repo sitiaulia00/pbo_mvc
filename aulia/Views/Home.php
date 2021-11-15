6afa7cba806247286e0908496471f5bbf7a553b9
sitiaulia00 <94333693+sitiaulia00@users.noreply.github.com>
2021-11-14 20:40:50 -0800

    pbo_pts
    Tugas aplikasi UTS Siti Aulia
    XII RPL 1

---
+++ aulia/Views/Home.php
@@ -0,0 +1,14 @@
+<body>
+
+    <div class="card text-light bg-danger p-2 text-dark bg-opacity-25">
+  <div class="card-header">Project PTS PBO</div>
+  <div class="card-body">
+    <p class="card-text">
+        Nama: Siti Aulia <br>
+        Kelas: XII RPL-1 <br>
+        No.Absen: 27
+    </p>
+  </div>
+</div>
+
+</body>
